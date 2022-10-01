eacd2685d0b639fccf697a39020f30be26b00a79
collinsmezie <collinschimezieobinwa@gmail.com>
2022-10-02 00:49:11 +0400

    fixed linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,12 +1,17 @@
-class Api::V1::GreetingsController < ApplicationController
-  
-  def index
-    id = rand(1..Greeting.count) 
-    if id.nil? 
-      render json: { error: 'sorry! not found' }.to_json, status: 404 
-    else @message = Greeting.find(id) 
-      render json: @message 
-    end 
-  end 
+# frozen_string_literal: true
 
+module Api
+  module V1
+    class GreetingsController < ApplicationController # rubocop:todo Style/Documentation
+      def index
+        id = rand(1..Greeting.count)
+        if id.nil?
+          render json: { error: 'sorry! not found' }.to_json, status: 404
+        else
+          @message = Greeting.find(id)
+          render json: @message
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    namespace :api do
-      namespace :v1 do
-        resources :greetings
-      end
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
     end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
